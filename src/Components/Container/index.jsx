9acc586296fbfd8da5975e3364cfe5b0b17946fc
Pablo Gomez <pablogomez0430@gmail.com>
2023-04-23 03:08:14 -0300

    before theme

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-
+import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
+import List from "../List";
 
+const StyledConteiner = styled.div `
+  background-color: ${({theme}) =>
+  theme.body
+};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StyledContent = styled.section `
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column; 
+  }
+`
 const Container = () => {
   return (
-    <div className="container">
+    <StyledConteiner>
       <Title>Smart Bank</Title>
-      <section className="content">
-        <Account />
-      </section>
-    </div>
+      <StyledContent >
+        <Account/>
+        <List/>
+       </StyledContent>
+    </StyledConteiner>
   );
 };
 
